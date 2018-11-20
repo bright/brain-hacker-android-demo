7f0abb523851b5ed9c995880eb1b728110f7af02
Andrzej Zabost <andrzej.zabost@brightinventions.pl>
2018-11-20 13:44:50 +0100

    Empty project

---
+++ app/src/main/java/pl/brightinventions/githubexplorer/IssuesActivity.kt
@@ -0,0 +1,12 @@
+package pl.brightinventions.githubexplorer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class IssuesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_issues)
+    }
+}
